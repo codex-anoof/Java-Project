e81ec144f61f197ea70837ebac22814772bce553
Lord Of Code <codejamx.anoof@gmail.com>
2025-04-26 12:03:03 +0530

    fix : fix some java code

--- src/main/java/cse/school/codejam/Transaction.java
+++ src/main/java/cse/school/codejam/Transaction.java
@@ -1,3 +1,4 @@
+
 package cse.school.codejam;
 
 import java.time.LocalDateTime;
@@ -12,30 +13,34 @@ public class Transaction {
     private final LocalDateTime timestamp = LocalDateTime.now();
 
     public Transaction setType(TransactionType type) {
-        this.type = type; return this;
+        this.type = type;
+        return this;
     }
 
     public Transaction setFromAccountNumber(String from) {
-        this.fromAccountNumber = from; return this;
+        this.fromAccountNumber = from;
+        return this;
     }
 
     public Transaction setToAccountNumber(String to) {
-        this.toAccountNumber = to; return this;
+        this.toAccountNumber = to;
+        return this;
     }
 
     public Transaction setAmount(double amt) {
-        if (amt == 0) throw new IllegalArgumentException("Amount must be positive.");
-        this.amount = amt; return this;
+        if (amt <= 0) throw new IllegalArgumentException("Amount must be greater than zero.");
+        this.amount = amt;
+        return this;
     }
 
     public String getTransactionDetails() {
         String msg = "";
         if (type == TransactionType.DEPOSIT) {
-            msg = "Deposit of " + amount + " to " + fromAccountNumber;
+            msg = "Deposit of " + amount + " to " + toAccountNumber;
         } else if (type == TransactionType.WITHDRAW) {
-            msg = "Withdrawal of " + amount + " from " + toAccountNumber;
+            msg = "Withdrawal of " + amount + " from " + fromAccountNumber;
         } else if (type == TransactionType.TRANSFER) {
-            msg = "Transfer of " + amount + " from " + toAccountNumber + " to " + fromAccountNumber;
+            msg = "Transfer of " + amount + " from " + fromAccountNumber + " to " + toAccountNumber;
         }
         return msg + " on " + timestamp;
     }
